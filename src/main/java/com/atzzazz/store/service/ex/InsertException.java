a810b6124c83dfe34063cc6db6d6806a345fd993
zzazz <zzazz@gmail.com>
2022-06-06 22:05:53 +0900

    service Class　の Exception　を定義

---
+++ src/main/java/com/atzzazz/store/service/ex/InsertException.java
@@ -0,0 +1,27 @@
+package com.atzzazz.store.service.ex;
+
+
+/**
+ * data を　insert　する際に生じる　Exception
+ */
+public class InsertException extends ServiceException{
+    public InsertException() {
+        super();
+    }
+
+    public InsertException(String message) {
+        super(message);
+    }
+
+    public InsertException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InsertException(Throwable cause) {
+        super(cause);
+    }
+
+    protected InsertException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/test/java/com/atzzazz/store/StoreApplicationTests.java
@@ -0,0 +1,27 @@
+package com.atzzazz.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void getConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+    }
+
+}
