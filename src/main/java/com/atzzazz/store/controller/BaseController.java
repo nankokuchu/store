ce0d081e97ed56538b73b455243ca952719ae00d
zzazz <zzazz@gmail.com>
2022-06-07 20:43:34 +0900

    ユーザーログイン機能実装

--- src/main/java/com/atzzazz/store/controller/BaseController.java
+++ src/main/java/com/atzzazz/store/controller/BaseController.java
@@ -1,8 +1,6 @@
 package com.atzzazz.store.controller;
 
-import com.atzzazz.store.service.ex.InsertException;
-import com.atzzazz.store.service.ex.ServiceException;
-import com.atzzazz.store.service.ex.UsernameDuplicateException;
+import com.atzzazz.store.service.ex.*;
 import com.atzzazz.store.util.JsonResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,10 +18,16 @@ public class BaseController {
         JsonResult<Void> result = new JsonResult<>(e);
         if (e instanceof UsernameDuplicateException){
             result.setCode(4000);
-            result.setMessage("サーバー側に未知のエラーが発生しています");
+            result.setMessage("ユーザー名が重複されています");
         }else if (e instanceof InsertException){
             result.setCode(5000);
             result.setMessage("サーバー側に未知のエラーが発生しています");
+        }else if (e instanceof PasswordNotMatchException){
+            result.setCode(6000);
+            result.setMessage("パースワードが間違っています");
+        }else if (e instanceof UserNotFoundException){
+            result.setCode(7000);
+            result.setMessage("ユーザーのデータが存在しません");
         }
         return result;
     }
--- src/main/java/com/atzzazz/store/controller/UserController.java
+++ src/main/java/com/atzzazz/store/controller/UserController.java
@@ -4,6 +4,7 @@ import com.atzzazz.store.pojo.User;
 import com.atzzazz.store.service.IUserService;
 import com.atzzazz.store.util.JsonResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -23,5 +24,16 @@ public class UserController extends BaseController {
         JsonResult<Void> result = new JsonResult<>(OK);
         result.setMessage("ユーザー登録成功しました");
         return result;
+    }
+
+    @RequestMapping("login")
+    public JsonResult<User> login(@RequestParam("username") String username,
+                                  @RequestParam("password") String password){
+        User user = userService.login(username, password);
+        JsonResult<User> userJsonResult = new JsonResult<>();
+        userJsonResult.setCode(OK);
+        userJsonResult.setData(user);
+        userJsonResult.setMessage("ログインできました");
+        return userJsonResult;
     }
 }
